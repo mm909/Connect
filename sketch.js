b44bbbf09b440adb93e92fe10a0d67fe6a413773
Mikian Musser <Bobar312@gmail.com>
2018-11-29 00:35:57 -0800

    Had lots of fun:)

--- sketch.js
+++ sketch.js
@@ -8,50 +8,55 @@ function roughDistance(x0, y0, x1, y1) {
 class Particle{
 	constructor(x,y){
 		this.pos = createVector(x,y);
-		this.vel = createVector(0,0.08);
+		this.vel = createVector(0,random(1,5));
+    this.r = random(255)
+    this.g = random(255)
+    this.b = random(255)
 	}
 
-	draw(){
-		rect(this.pos.x,this.pos.y,5,5);
-	}
 
-	fall(target){
-		this.follow(target);
+	fall(){
 		if(this.pos.y > height){
 			 this.pos.y = 0;
+       this.pos.x = random(0,width);
+       this.vel = createVector(0,random(1,5));
 		}
-		this.pos.add(this.vel);
+    this.pos.add(this.vel);
 	}
 
-	follow(target){
-		// this.draw();
-		let distance_valid =150;
+	string(target){
+		let distance_valid = 300;
+    let currDistance = roughDistance(this.pos.x,this.pos.y,target.x,target.y)
 		if(roughDistance(this.pos.x,this.pos.y,target.x,target.y) <= distance_valid){
-			this.draw();
-			stroke(255,255,255);
+      let c = map(currDistance, 0, distance_valid, 200, 0)
+			stroke(255,255,255,c);
 			line(this.pos.x,this.pos.y,target.x,target.y);
+      return true;
 		}
+    return false;
 	}
 }
 
 let particles = [];
-let total = 200;
-let miuseData;
+let total = 600;
 function setup() {
   createCanvas(windowWidth, windowHeight);
 	for(let i=0;i<total;i++){
-		particles[i] = new Particle(random(width,-width),random(-height,height));
-
+		particles[i] = new Particle(random(0,width),random(0,height));
 	}
-
-
 }
+
 let i = 0;
 function draw() {
-  background(50);
+  background(50,210);
 	for(let i=0;i<particles.length;i++){
 		for(let j=0;j<particles.length - i;j++){
-			particles[j].fall(particles[i].pos)
+      if(i!=j) if(particles[j].string(particles[i].pos)) break;
 		}
+    particles[i].fall()
+    particles[i].string({
+      x: mouseX,
+      y: mouseY
+    })
 	}
 }
